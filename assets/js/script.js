80260cc91997eb15354cc0d6118bf06d1211cd5a
Johnathonanon <jconry21@gmail.com>
2021-10-08 15:16:30 +0000

    Add function for username and difficulty submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,17 @@
-// Waits for DOM to finish loading and requests user input username
+// Allows input of username and difficulty choice
+document.getElementById("enter").addEventListener("click", function() {
+    let username = document.getElementById("username").value;
+    if (username === "") {
+        alert("Please enter a valid username");
+    } else {
+    document.getElementById("message").innerHTML = `
+    <p>Hi ${username}! Please choose a difficulty</p>
+    <button id="easy" class="button">Easy</button>
+    <button id="medium" class="button">Medium</button>
+    <button id="hard" class="button">Hard</button>
+    `;
+    document.getElementById("username").remove();
+    document.getElementById("enter").remove();
+}});
+
+
