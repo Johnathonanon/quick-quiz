078f007f44cd89db721c9114ca70829a3375e57b
Johnathonanon <jconry21@gmail.com>
2021-10-11 13:26:10 +0000

    Troubleshoot buggy javascript'

--- assets/js/script.js
+++ assets/js/script.js
@@ -92,21 +92,20 @@ let questions = [{
     }
 ];
 
-// Allows input of username and difficulty choice
+// Creates username and advances to difficulty choice on confirmation
 
 document.getElementById("enter").addEventListener("click", function () {
     let username = document.getElementById("username").value;
+    document.getElementById("welcome-area").innerHTML =`
+    <p>Hi ${username}! Please choose a difficulty</p>
+    <button id="easy" class="button">Easy</button>
+    <button id="medium" class="button">Medium</button>
+    <button id="hard" class="button">Hard</button>
+    `;
     if (username === "") {
         alert("Please enter a valid username");
-    } else {
-        document.getElementById("message").innerHTML = `
-                <p>Hi ${username}! Please choose a difficulty</p>
-                <button id="easy" class="button">Easy</button>
-                <button id="medium" class="button">Medium</button>
-                <button id="hard" class="button">Hard</button>
-                `;
-        document.getElementById("username").remove();
-        document.getElementById("enter").remove();
     }
 });
+
+// Generates questions based on difficulty choice
 
